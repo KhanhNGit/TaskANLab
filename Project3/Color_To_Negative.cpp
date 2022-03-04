add16f894596c8684b734733f139f7072bdcbf08
KhanhNGit <99749800+KhanhNGit@users.noreply.github.com>
2022-03-04 08:18:44 +0700

    Convert Image (add bitwise_not)

--- Project3/Color_To_Negative.cpp
+++ Project3/Color_To_Negative.cpp
@@ -5,11 +5,7 @@ public:
 	Color_To_Negative(std::string src) :Convert_Image(src){}
 	void Display() {
 		Mat image = imread(GetSrc(), IMREAD_COLOR);
-		for (int i = 0;i < image.rows;i++) {
-			for (int j = 0;j < image.cols;j++) {
-				image.at<uchar>(i, j) = 255 - image.at<uchar>(i, j);
-			}
-		}
+		bitwise_not(image, image);
 		namedWindow("Color_To_Negative", 1);
 		imshow("Color_To_Negative", image);
 		waitKey(0);
